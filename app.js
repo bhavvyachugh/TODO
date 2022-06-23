7fd9043da348a309b8670c4268ad26b64198a40d
Ayush Chugh <ayushchugh2006@gmail.com>
2022-06-23 23:29:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+import path from 'path';
+
+config();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true })); // to parse form data
+app.use(express.static(path.resolve('static'))); // to tell express where static files are present
+app.set('view engine', 'ejs'); // to tell express which view template engine to use
+app.set('views', path.resolve('views')); // to tell express where views are present
+
+mongoose.connect(process.env.DB_URI, () => {
+	console.log('connected to DB');
+
+	app.listen(8000);
+});
