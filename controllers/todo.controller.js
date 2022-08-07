673c43f3f68695a1df7cfd55e68362c5899099d6
Bhavya Chugh <bhavvyachugh@gmail.com>
2022-08-07 11:17:45 +0530

    update todo

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,61 +1,82 @@
-import Todo from '../models/todo.model.js';
-import User from '../models/user.model.js';
+import Todo from "../models/todo.model.js";
+import User from "../models/user.model.js";
 
 export const postTodoHandler = async (req, res) => {
-	const { title, description } = req.body;
-	const loggedInUserId = res.locals.user;
+  const { title, description } = req.body;
+  const loggedInUserId = res.locals.user;
 
-	if (!title || !description)
-		return res.status(401).send('Please provide title and description');
+  if (!title || !description)
+    return res.status(401).send("Please provide title and description");
 
-	const newTodo = await Todo.create({
-		title,
-		description,
-	});
+  const newTodo = await Todo.create({
+    title,
+    description,
+  });
 
-	const loggedInUser = await User.findById(loggedInUserId);
+  const loggedInUser = await User.findById(loggedInUserId);
 
-	if (!loggedInUser) return res.status(401).send('Please login to create');
+  if (!loggedInUser) return res.status(401).send("Please login to create");
 
-	loggedInUser.tasks.push(newTodo._id);
+  loggedInUser.tasks.push(newTodo._id);
 
-	loggedInUser.save();
+  loggedInUser.save();
 
-	res.redirect('/');
+  res.redirect("/");
 };
 
 export const getHomeHandler = async (req, res) => {
-	const loggedInUserId = res.locals.user;
+  const loggedInUserId = res.locals.user;
 
-	const user = await User.findById(loggedInUserId).populate('tasks');
+  const user = await User.findById(loggedInUserId).populate("tasks");
 
-	res.render('home', {
-		isUserLoggedIn: res.locals.user ? true : false,
-		todos: user && user.tasks.length > 0 ? user.tasks : [],
-	});
+  res.render("home", {
+    isUserLoggedIn: res.locals.user ? true : false,
+    todos: user && user.tasks.length > 0 ? user.tasks : [],
+  });
 };
 
 // UPdate Todo
 
+export const updateTodoHandler = async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  try {
+    await Todo.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+      },
+      {
+        new: true,
+      }
+    );
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send("Internal server error");
+  }
+};
+
 export const deleteTodoHandler = async (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	try {
-		await Todo.findByIdAndDelete(id);
+  try {
+    await Todo.findByIdAndDelete(id);
 
-		const userId = res.locals.user;
+    const userId = res.locals.user;
 
-		const userFromDB = await User.findById(userId);
+    const userFromDB = await User.findById(userId);
 
-		const filteredTasks = userFromDB.tasks.filter(
-			task => task.toString() !== id
-		);
+    const filteredTasks = userFromDB.tasks.filter(
+      (task) => task.toString() !== id
+    );
 
-		userFromDB.tasks = filteredTasks;
-		userFromDB.save();
+    userFromDB.tasks = filteredTasks;
+    userFromDB.save();
 
-		res.redirect('/');
-	} catch (err) {
-		res.status(500).send(err);
-	}
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,14 +1,16 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-	deleteTodoHandler,
-	getHomeHandler,
-	postTodoHandler,
-} from '../controllers/todo.controller.js';
+  deleteTodoHandler,
+  getHomeHandler,
+  postTodoHandler,
+  updateTodoHandler,
+} from "../controllers/todo.controller.js";
 
 const todoRouter = Router();
 
-todoRouter.get('/', getHomeHandler);
-todoRouter.post('/todo', postTodoHandler);
-todoRouter.post('/todo/delete/:id', deleteTodoHandler);
+todoRouter.get("/", getHomeHandler);
+todoRouter.post("/todo", postTodoHandler);
+todoRouter.post("/todo/delete/:id", deleteTodoHandler);
+todoRouter.post("/todo/update/:id", updateTodoHandler);
 
 export default todoRouter;
